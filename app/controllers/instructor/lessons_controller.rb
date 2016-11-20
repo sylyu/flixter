6e18b5554d1e3b2d9c80f0f1c705d7ed638e22bb
Sylyu <sylyu>
2016-11-20 19:50:16 +0000

    locking down permissions and refactoring lessons controller

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,16 +1,36 @@
 class Instructor::LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
+  
   def new
-    @section = Section.find(params[:section_id])
     @lesson = Lesson.new
   end
 
   def create
-    @section = Section.find(params[:section_id])
-    @lesson = @section.lessons.create(lesson_params)
-    redirect_to instructor_course_path(@section.course)
+    @lesson = current_section.lessons.create(lesson_params)
+    redirect_to instructor_course_path(current_section.course)
   end
 
   private
+
+   def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      return render text: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+  # To let Rails know it can let the view call the method
+  def helper_method :current_session
+
+  def current_session
+    # if @current_section == nil
+    #   @current_section = Section.find(params[:section_id])
+    #   @current_section
+    # else
+    #   @current_section
+    # end
+    @current_session ||= Section.find(params[:section_id])
+  end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
