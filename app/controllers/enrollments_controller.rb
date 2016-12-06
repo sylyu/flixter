3e229165e6461f84a76091b6738b450f15d53400
Sylyu <sylyu>
2016-12-06 00:01:44 +0000

    change line 5 course.premium? to current_course.premium? for enrollments_controller.rb, add missing end statement to lessons_controller.rg on line 14

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,28 +2,27 @@ class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    if course.premium?
-    # Amount in cents
-    @amount = (current_course.cost * 100).to_i
-
-    customer = Stripe::Customer.create(
-      :email => current_user.email,
-      :card  => params[:stripeToken]
+    if current_course.premium?
+      @amount = (current_course.cost * 100).to_i
+      
+      customer = Stripe::Customer.create(
+        :email => current_user.email,
+        :card  => params[:stripeToken]
     )
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Flixter Premo Content',
-      :currency    => 'usd'
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Flixter Premo Content',
+        :currency    => 'usd'
     )
-  end
+    end
 
-  current_user.enrollments.create(course: current_course)
-  redirect_to course_path(current_course)
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to root_path
+    current_user.enrollments.create(course: current_course)
+    redirect_to course_path(current_course)
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to root_path
 
   end
 
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,13 +7,15 @@ class LessonsController < ApplicationController
 
   private
 
-   def require_authorized_for_current_lesson
+  def require_authorized_for_current_lesson
     if !current_user.enrolled_in?(current_lesson.section.course)
       redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled to view the course'
     end
+  end
   
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
 end
